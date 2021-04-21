9d7bef6b595634e124e2a003e36788808ce3d3de
Cameron Brantley <brantley.cameron@gmail.com>
2021-04-20 20:41:59 -0500

    JS More Primitives

---
+++ ClassActivities/2.2.3-JSMorePrimitives.js
@@ -0,0 +1,28 @@
+// create a variable "timeOfDay" and assign it a string
+let timeOfDay = "evening";
+// create a variable "greeting" that references a template literal
+let greeting = `I hope you are having a wonderful ${timeOfDay}!!!!`;
+console.log(greeting);
+// use "timeOfDay" in the template literal to create a message like "Good morning!" or "Good evening!"
+
+// print "greeting"
+
+// create a new variable, but do not assign it a value
+let savvyCoders;
+// print the variable and its type
+// ? what type should we expect?
+console.log(savvyCoders);
+console.log(typeof savvyCoders);
+// assign the variable a value that indicates the variable is purposely blank
+// ? what value should we assign?
+savvyCoders = null;
+// print the variable and its type again
+// ? what type should we expect?
+console.log(savvyCoders);
+console.log(typeof savvyCoders);
+// try to print a variable that does not exist
+// ? what should we expect to print in the CLI?
+// console.log(javaScript);
+// print "greeting" again
+// ? will this line run?
+console.log(greeting);
